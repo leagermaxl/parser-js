c64bf737567ee2fb5de8c59b2cabd46d85406990
volot <volotkovskiy.dmitriy@gmail.com>
2025-02-01 01:17:21 +0200

    db updated

--- src/db/db.js
+++ src/db/db.js
@@ -1,36 +1,20 @@
+import 'dotenv/config';
 import mongoose from 'mongoose';
+import { Orders } from './default_model.js';
 
-const uri =
-  'mongodb+srv://<username>:<password>@cluster0.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const uri = process.env.MONGO_URI;
+console.log(uri);
 
-mongoose
-  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('Connected to MongoDB Atlas'))
-  .catch((err) => console.error('Connection error:', err));
-
-const Schema = mongoose.Schema;
-
-const allTargets = new Schema({
-  orderNumbers: { orderId: Number, orderNum: Number },
-  orderDate: Date,
-  totalAmount: String,
-  amoutWithCoupon: String,
-  products: [
-    {
-      quantity: Number,
-      name: String,
-    },
-  ],
-  coupon: { discountPercent: String, code: String },
-});
-
-const Models = mongoose.model('allTargets', allTargets);
-
-async function fetchAndSaveData() {
-  const data = await FUNC_DATA();
-  const newEntry = new Models(DATA);
-  await newEntry.save();
-  console.log('Data saved to MongoDB Atlas!');
+export async function connectToDatabase() {
+  try {
+    await mongoose.connect(uri);
+    console.log('Connected to MongoDB Atlas');
+  } catch (err) {
+    console.error('Connection error:', err);
+  }
 }
 
-fetchAndSaveData();
+export async function fetchAndSaveDataToDB(data) {
+  const newEntry = await Orders.insertMany(data);
+  console.log('Data saved to MongoDB Atlas!', newEntry);
+}
---
+++ src/db/default_model.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+const orderSchema = new Schema(
+  {
+    orderNumbers: { orderId: Number, orderNum: Number },
+    orderDate: String,
+    totalAmount: String,
+    amoutWithCoupon: String,
+    products: [
+      {
+        quantity: Number,
+        name: String,
+        _id: false, 
+      },
+    ],
+    coupon: { discountPercent: String, code: String },
+  },
+  { versionKey: false },
+);
+
+export const Orders = mongoose.model('orderSchema ', orderSchema);
