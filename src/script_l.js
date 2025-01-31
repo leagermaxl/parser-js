cf2bb2c023a6312e84731d81e07b2cda3c0ddb39
Max Lyubchenko <00eagermax00@gmail.com>
2025-02-01 01:32:41 +0200

    fix bugs

--- src/script_l.js
+++ src/script_l.js
@@ -1,6 +1,6 @@
+import { connectToDatabase, fetchAndSaveDataToDB } from './db/db.js';
 import { requestsForOrders } from './utils/utils.js';
 import { createStyledExcel } from './utils/xlsx.js';
-import { connectToDatabase, fetchAndSaveDataToDB } from './db/db.js';
 
 const urlPage =
   'https://cp21.megagroup.ru/-/cms/v1/shop2/order/?shop_id=4373441&ver_id=1586314&access=u%3B1623555&p=0';
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,21 +1,18 @@
 import { processFetchData, processFetchDataPages } from '../parser/parserUtils.js';
 
-
-
 export async function processOrders(orderIds, urlOrder) {
   const ordersData = [];
-  for (let i = 0; i < Math.min(orderIds.length, 2); i++) {
+  for (let i = orderIds.length - 1; i >= 0; i--) {
+    // for (let i = 0; i < Math.min(orderIds.length, 50); i++) {
     console.log('myCallback');
     const updatedUrl = urlOrder.replace(/(order_id=)\d+/, `$1${orderIds[i].orderId}`);
     console.log('[REQUEST TO]:', `${updatedUrl}`);
 
-    ordersData[i] = await processFetchData(updatedUrl, true);
+    ordersData.push(await processFetchData(updatedUrl, true));
 
-    console.log(ordersData[i]);
+    if (i === orderIds.length - 4) return ordersData;
 
-    if (i === orderIds.length - 1) return;
-
-    const { max, min } = { max: 5000, min: 500 };
+    const { max, min } = { max: 3000, min: 500 };
     const randomInterval = Math.floor(Math.random() * (max - min + 1)) + 500;
 
     await new Promise((resolve) => setTimeout(resolve, randomInterval));
@@ -24,15 +21,14 @@ export async function processOrders(orderIds, urlOrder) {
 }
 
 export async function requestsForOrders(urlPage, urlOrder) {
-  console.log('requestsForOrders');
-  let page = 0;
+  let page = 2;
   const ordersData = [];
 
   // const { pages } = await processFetchDataPages(urlPage, true);
   const pages = 2;
   console.log(pages);
 
-  while (page < pages) {
+  while (page >= 0) {
     console.log('while');
     console.log('page', page);
     const updatedUrlPage = urlPage.replace(/(p=)\d+/, `$1${page}`);
@@ -42,7 +38,8 @@ export async function requestsForOrders(urlPage, urlOrder) {
 
     ordersData.push(...(await processOrders(orderIds, urlOrder)));
 
-    page++;
+    page--;
   }
+  console.log('ordersData', ordersData);
   return ordersData;
 }
